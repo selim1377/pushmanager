54542a2d20a05280a6eefb2acac87952ebe27757
Selim Bakdemir <selim.bakdemir@gmail.com>
2014-02-03 10:04:49 +0200

    Initial Commit

---
+++ PushManager/PushManager.h
@@ -0,0 +1,13 @@
+//
+//  PushManager.h
+//  PushManager
+//
+//  Created by Selim on 03.02.14.
+//  Copyright (c) 2014 Concept Factory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PushManager : NSObject
+
+@end
---
+++ PushManager/PushManager.m
@@ -0,0 +1,13 @@
+//
+//  PushManager.m
+//  PushManager
+//
+//  Created by Selim on 03.02.14.
+//  Copyright (c) 2014 Concept Factory. All rights reserved.
+//
+
+#import "PushManager.h"
+
+@implementation PushManager
+
+@end
